c82fa2528bf3dc9f6c56780bfb4b18618375d597
aerexio <105903639+aerexio@users.noreply.github.com>
2022-05-20 00:12:31 -0500

    Add files via upload

---
+++ Torres_Kevin_DDL.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE torres.Customer(
+    customer_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    CustomerFirstName CHAR(50) NOT NULL,
+    CustomerLastName CHAR(50) NOT NULL,
+    CustomerAddress VARCHAR(50) NOT NULL,
+    CustomerCity CHAR(50) NOT NULL,
+    CustomerState CHAR(50) NOT NULL
+    );
+CREATE TABLE torres.Employee(
+    employee_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    EmployeeFirstName CHAR(50) NOT NULL,
+    EmployeeLastName CHAR(50) NOT NULL,
+    EmployeeBirthDate VARCHAR(50) NOT NULL,
+    );
+CREATE TABLE torres.Pizza(
+    pizza_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    Pizza_size CHAR(50) NOT NULL,
+    Pizza_type CHAR(50) NOT NULL
+    );
+CREATE TABLE torres.Orders(
+    order_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    customer_id_fk INT NOT NULL FOREIGN KEY REFERENCES torres.Customer(customer_id),
+    pizza_id_fk INT NOT NULL FOREIGN KEY REFERENCES torres.Pizza(pizza_id),
+    employee_id_fk INT NOT NULL FOREIGN KEY REFERENCES torres.Employee(employee_id),
+    Order_date VARCHAR(50) NOT NULL,
+    Quantityordered FLOAT(10) NOT NULL,
+    Order_price FLOAT(4) NOT NULL
+    );
---
+++ Torres_Kevin_Insert.sql
@@ -0,0 +1,40 @@
+﻿INSERT INTO torres.Customer(CustomerFirstName,CustomerLastName,CustomerAddress, CustomerState,CustomerCity)
+VALUES 
+('Bruce','Wayne','3273 S West st', 'Wisconsin','Milwaukee'),
+('Ellie','Miller','3324 N Beck st', 'Wisconsin','Waukesha'),
+('Peter','Parker','3273 W Queen st', 'Wisconsin','Milwaukee'),
+('America','Chavez','5476 S Bay st','Wisconsin','Milwaukee'),
+('John','Wick','9304 E Washington st', 'Wisconsin','Milwaukee'),
+('Lara','Croft','1290 N West st', 'Wisconsin','Bay View'),
+('John','Mulaney','5784 E West st', 'Wisconsin','Bay View');
+
+
+
+INSERT INTO torres.Employee (EmployeeFirstName,EmployeeLastName,EmployeeBirthDate)
+VALUES
+('Jake','Peralta',2000-01-01),
+('Trepe', 'Smith',1990-02-10),
+('Joel','Miller',1986-02-08),
+('Leo','Gonzales',1998-03-10),
+('Diego','Hernandez',1995-05-28),
+('Tony','Sinantra',1992-06-15),
+('Tania','Kress', 1972-11-23);
+
+INSERT INTO torres.Pizza(Pizza_size,Pizza_type)
+VALUES
+('Large','Sausage'),
+('Large','Pepperoni'),
+('Small','Chicken'),
+('Small','Sausage'),
+('Medium','Pepperoni'),
+('Medium','Cheese'),
+('Large','Cheese');
+INSERT INTO torres.orders (Order_date,Quantityordered,Order_price)
+VALUES
+(2019-05-04,05,50.37),
+(2020-06-03,03,30.29),
+(2020-07-05,02,20.29),
+(2020-09-20,01,10.10),
+(2020-10-01,06,63.29),
+(2020-12-04,04,40.29),
+(2020-12-08,12,128.50);
